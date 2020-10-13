51311d366940048bd9f5dcf36fef88526a567365
Robert Englund <kastanjen3@gmail.com>
2020-10-13 19:59:47 +0200

    Added comments to methods

--- src/main/java/se/su/dsv/inte/projektarbete/weapons/Weapon.java
+++ src/main/java/se/su/dsv/inte/projektarbete/weapons/Weapon.java
@@ -14,9 +14,17 @@ public class Weapon {
     private int durability;
 
     // Constructors
+    /**
+     * Constructor with full range of values
+     * @param name String, the name
+     * @param description String, the description
+     * @param baseDamage int, the base damage dealt (>0)
+     * @param range int, the range of weapon (>0)
+     * @param durability int, the current durability (0-100)
+     */
     public Weapon(String name, String description, int baseDamage, int range, int durability) {
 
-        // Check values
+        // Verify base damage value
         if ( baseDamage <= 0 ) {
             throw new IllegalArgumentException("Base Damage cannot be less than or equal to zero.");
         }
@@ -28,23 +36,54 @@ public class Weapon {
         this.durability = durability;
     }
 
+    /**
+     * Constructor with minimal range of values
+     * @param name String, the name
+     * @param description String, the description
+     * @param baseDamage int, the base damage dealt (>0)
+     * @param range int, the range of weapon (>0)
+     */
     public Weapon(String name, String description, int baseDamage, int range) {
         this(name, description, baseDamage, range, 100);
     }
 
     // Methods
+    /**
+     * Get weapon name
+     * @return String, the name
+     */
     public String getName() {
         return this.name;
     }
+
+    /**
+     * Get weapon description
+     * @return String, the description
+     */
     public String getDescription() {
         return this.description;
     }
+
+    /**
+     * Get the base damage
+     * @return int, the base damage
+     */
     public int getBaseDamage() {
         return this.baseDamage;
     }
+
+    /**
+     * Get the range
+     * @return int, the range
+     */
     public int getRange() {
         return this.range;
     }
+
+    /**
+     * Get the durability
+     * @return int, the durability
+     */
     public int getDurability() {
         return this.durability;
     }
