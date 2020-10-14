ab9a7b33aa55ebacfebcc094e87e94b2c3b7deec
Robert Englund <kastanjen3@gmail.com>
2020-10-14 13:20:33 +0200

    Flera tester för Weapons klassen
    Skapade flera tester för Weapns klassen och utökade den med modifiers och element som de kan attackera.
    
    Skapade därför ett interface för Modifiers för att kunna skapa olika modifiers i framtiden. Är inte säker på att detta är lösningen som bör göras, kanske kommer göra om WeaponsModifier, får fundera mer när jag kommer till att testa den.

--- src/main/java/se/su/dsv/inte/projektarbete/weapons/Weapon.java
+++ src/main/java/se/su/dsv/inte/projektarbete/weapons/Weapon.java
@@ -1,5 +1,8 @@
 package se.su.dsv.inte.projektarbete.weapons;
 
+import se.su.dsv.inte.projektarbete.ElementType;
+import se.su.dsv.inte.projektarbete.weapons.modifiers.WeaponModifier;
+
 /**
  * Class to represent a weapon at the highest level.
  */
@@ -13,6 +16,9 @@ public class Weapon {
     private int range;
     private int durability;
 
+    private ElementType[] canAttackElementTypes;
+    private WeaponModifier modifier;
+
     // Constructors
     /**
      * Constructor with full range of values
@@ -20,20 +26,62 @@ public class Weapon {
      * @param description String, the description
      * @param baseDamage int, the base damage dealt (>0)
      * @param range int, the range of weapon (>0)
+     * @param canAttackElementTypes ElementType[], alla the element types that this weapon can attack
      * @param durability int, the current durability (0-100)
+     * @param modifier WeaponModifier, the modifiers for this weapon
      */
-    public Weapon(String name, String description, int baseDamage, int range, int durability) {
+    public Weapon(String name, String description, int baseDamage, int range, ElementType[] canAttackElementTypes, int durability, WeaponModifier modifier) {
 
         // Verify base damage value
         if ( baseDamage <= 0 ) {
             throw new IllegalArgumentException("Base Damage cannot be less than or equal to zero.");
+        }
+        // Verify range value
+        else if ( range <= 0 ) {
+            throw new IllegalArgumentException("Range cannot be less than or equal to zero.");
+        }
+        // Verify can attack elements
+        else if ( canAttackElementTypes == null || canAttackElementTypes.length == 0 ) {
+            throw new IllegalArgumentException("List with elements that the weapon can attack must have enum values");
+        }
+        // Verify durability values
+        else if ( durability < 0 || durability > 100 ) {
+            throw new IllegalArgumentException("Durability cannot be less than zero or greater then one hundred.");
         }
 
         this.name = name;
         this.description = description;
         this.baseDamage = baseDamage;
         this.range = range;
+        this.canAttackElementTypes = canAttackElementTypes;
         this.durability = durability;
+        this.modifier = modifier;
+    }
+
+    /**
+     * Constructor with no modifier but durability
+     * @param name String, the name
+     * @param description String, the description
+     * @param baseDamage int, the base damage dealt (>0)
+     * @param range int, the range of weapon (>0)
+     * @param canAttackElementTypes ElementType[], alla the element types that this weapon can attack
+     * @param durability int, the current durability (0-100)
+     */
+    public Weapon(String name, String description, int baseDamage, int range, ElementType[] canAttackElementTypes, int durability) {
+        this(name, description, baseDamage, range, canAttackElementTypes, durability, null);
+    }
+
+    /**
+     * Constructor with no durability but modifier
+     * @param name String, the name
+     * @param description String, the description
+     * @param baseDamage int, the base damage dealt (>0)
+     * @param range int, the range of weapon (>0)
+     * @param canAttackElementTypes ElementType[], alla the element types that this weapon can attack
+     * @param modifier WeaponModifier, the modifier for that weapon
+     */
+    public Weapon(String name, String description, int baseDamage, int range, ElementType[] canAttackElementTypes, WeaponModifier modifier) {
+        this(name, description, baseDamage, range, canAttackElementTypes, 100, modifier);
     }
 
     /**
@@ -42,10 +90,12 @@ public class Weapon {
      * @param description String, the description
      * @param baseDamage int, the base damage dealt (>0)
      * @param range int, the range of weapon (>0)
+     * @param canAttackElementTypes ElementType[], alla the element types that this weapon can attack
      */
-    public Weapon(String name, String description, int baseDamage, int range) {
-        this(name, description, baseDamage, range, 100);
+    public Weapon(String name, String description, int baseDamage, int range, ElementType[] canAttackElementTypes) {
+        this(name, description, baseDamage, range, canAttackElementTypes, 100, null);
     }
+
 
     // Methods
     /**
@@ -86,6 +136,22 @@ public class Weapon {
      */
     public int getDurability() {
         return this.durability;
+    }
+
+    /**
+     * Get the element types this weapon can attack
+     * @return ElementType[], the elements this weapon can attack
+     */
+    public ElementType[] getCanAttackElementTypes() {
+        return this.canAttackElementTypes;
+    }
+
+    /**
+     * Get the weapon modifier
+     * @return WeaponModifier, the modifier
+     */
+    public WeaponModifier getModifier() {
+        return this.modifier;
     }
 
 }
--- src/test/java/se/su/dsv/inte/projektarbete/weapons/WeaponTest.java
+++ src/test/java/se/su/dsv/inte/projektarbete/weapons/WeaponTest.java
@@ -1,6 +1,8 @@
 package se.su.dsv.inte.projektarbete.weapons;
 
 import org.junit.jupiter.api.Test;
+import se.su.dsv.inte.projektarbete.ElementType;
+import se.su.dsv.inte.projektarbete.weapons.modifiers.WeaponModifier;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -18,14 +20,19 @@ public class WeaponTest {
         String desc = "Det första svärdet som skapades.";
         int baseDamage = 10;
         int range = 1;
+        ElementType[] canAttack = {ElementType.LAND, ElementType.WATER};
         int durability = 100;
-        Weapon w = new Weapon(name, desc, baseDamage, range, durability);
+        WeaponModifier modifer = null;
+
+        Weapon w = new Weapon(name, desc, baseDamage, range, canAttack, durability, modifer);
 
         assertEquals(name, w.getName());
         assertEquals(desc, w.getDescription());
         assertEquals(baseDamage, w.getBaseDamage());
         assertEquals(range, w.getRange());
+        assertEquals(canAttack, w.getCanAttackElementTypes());
         assertEquals(durability, w.getDurability());
+        assertEquals(modifer, w.getModifier());
 
     }
 
@@ -38,13 +45,63 @@ public class WeaponTest {
         String desc = "Det andra svärdet som skapades.";
         int baseDamage = 12;
         int range = 2;
-        Weapon w = new Weapon(name, desc, baseDamage, range);
+        ElementType[] canAttack = {ElementType.LAND, ElementType.WATER};
+
+        Weapon w = new Weapon(name, desc, baseDamage, range, canAttack);
 
         assertEquals(name, w.getName());
         assertEquals(desc, w.getDescription());
         assertEquals(baseDamage, w.getBaseDamage());
         assertEquals(range, w.getRange());
+        assertEquals(canAttack, w.getCanAttackElementTypes());
         assertEquals(100, w.getDurability());
+        assertEquals(null, w.getModifier());
+    }
+
+    /**
+     * Test the constructor with minimal correct values plus durability.
+     */
+    @Test
+    public void testConstructorSetMinimalValuesPlusDurability() {
+        String name = "Svärd 3";
+        String desc = "Det tredje svärdet som skapades.";
+        int baseDamage = 12;
+        int range = 2;
+        ElementType[] canAttack = {ElementType.LAND, ElementType.WATER};
+        int durability = 100;
+
+        Weapon w = new Weapon(name, desc, baseDamage, range, canAttack, durability);
+
+        assertEquals(name, w.getName());
+        assertEquals(desc, w.getDescription());
+        assertEquals(baseDamage, w.getBaseDamage());
+        assertEquals(range, w.getRange());
+        assertEquals(canAttack, w.getCanAttackElementTypes());
+        assertEquals(durability, w.getDurability());
+        assertEquals(null, w.getModifier());
+    }
+
+    /**
+     * Test the constructor with minimal correct values plus weapon modifier.
+     */
+    @Test
+    public void testConstructorSetMinimalValuesPlusWeaponModifier() {
+        String name = "Svärd 4";
+        String desc = "Det fjärde svärdet som skapades.";
+        int baseDamage = 12;
+        int range = 2;
+        ElementType[] canAttack = {ElementType.LAND, ElementType.WATER};
+        WeaponModifier modifier = null;
+
+        Weapon w = new Weapon(name, desc, baseDamage, range, canAttack, modifier);
+
+        assertEquals(name, w.getName());
+        assertEquals(desc, w.getDescription());
+        assertEquals(baseDamage, w.getBaseDamage());
+        assertEquals(range, w.getRange());
+        assertEquals(canAttack, w.getCanAttackElementTypes());
+        assertEquals(100, w.getDurability());
+        assertEquals(modifier, w.getModifier());
     }
 
     /**
@@ -52,14 +109,127 @@ public class WeaponTest {
      */
     @Test
     public void testConstructorBaseDamageEqualToZero() {
-        String name = "Svärd 3";
-        String desc = "Det tredje svärdet som skapades.";
+        String name = "Svärd 5";
+        String desc = "Det femte svärdet som skapades.";
         int baseDamage = 0;
         int range = 3;
+        ElementType[] canAttack = {ElementType.LAND, ElementType.WATER};
 
-        assertThrows( IllegalArgumentException.class, ()-> {
-            Weapon w = new Weapon(name, desc, baseDamage, range);
+        assertThrows( IllegalArgumentException.class, () -> {
+            new Weapon(name, desc, baseDamage, range, canAttack);
         });
+    }
+
+    /**
+     * Test the constructor with a base damage of -1.
+     */
+    @Test
+    public void testConstructorBaseDamageLessThanZero() {
+        String name = "Svärd 6";
+        String desc = "Det sjätte svärdet som skapades.";
+        int baseDamage = -1;
+        int range = 4;
+        ElementType[] canAttack = {ElementType.LAND, ElementType.WATER};
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            new Weapon(name, desc, baseDamage, range, canAttack);
+        });
+    }
+
+    /**
+     * Test the constructor with a range of 0.
+     */
+    @Test
+    public void testConstructorRangeEqualToZero() {
+        String name = "Svärd 7";
+        String desc = "Det sjunde svärdet som skapades.";
+        int baseDamage = 2;
+        int range = 0;
+        ElementType[] canAttack = {ElementType.LAND, ElementType.WATER};
+
+        assertThrows( IllegalArgumentException.class, () -> {
+            new Weapon(name, desc, baseDamage, range, canAttack);
+        } );
+    }
+
+    /**
+     * Test the constructor with a range of -1.
+     */
+    @Test
+    public void testConstructorRangeLessThanZero() {
+        String name = "Svärd 8";
+        String desc = "Det åttonde svärdet som skapades.";
+        int baseDamage = 3;
+        int range = -1;
+        ElementType[] canAttack = {ElementType.LAND, ElementType.WATER};
+
+        assertThrows( IllegalArgumentException.class, () ->{
+            new Weapon(name, desc, baseDamage, range, canAttack);
+        } );
+    }
+
+    /**
+     * Test the constructor with a durability of -1.
+     */
+    @Test
+    public void testConstructorDurabilityLessThanZero() {
+        String name = "Svärd 9";
+        String desc = "Det nionde svärdet som skapades.";
+        int baseDamage = 4;
+        int range = 1;
+        ElementType[] canAttack = {ElementType.LAND, ElementType.WATER};
+        int durability = -1;
+
+        assertThrows( IllegalArgumentException.class, () -> {
+            new Weapon(name, desc, baseDamage, range, canAttack, durability);
+        } );
+    }
+
+    /**
+     * Test the constructor with a durability of 101.
+     */
+    @Test
+    public void testConstructorDurabilityGreaterThanOneHundred() {
+        String name = "Svärd 10";
+        String desc = "Det tionde svärdet som skapades.";
+        int baseDamage = 10;
+        int range = 4;
+        ElementType[] canAttack = {ElementType.LAND, ElementType.WATER};
+        int durability = 101;
+
+        assertThrows( IllegalArgumentException.class, () -> {
+            new Weapon(name, desc, baseDamage, range, canAttack, durability);
+        } );
+    }
+
+    /**
+     * Test the constrcutor with a can attack value of null.
+     */
+    @Test
+    public void testConstructorCanAttackElementTypesNull() {
+        String name = "Svärd 11";
+        String desc = "Det elfte svärdet som skapades.";
+        int baseDamage = 10;
+        int range = 2;
+        ElementType[] canAttack = null;
+
+        assertThrows( IllegalArgumentException.class, () -> {
+            new Weapon(name, desc, baseDamage, range, canAttack);
+        });
+    }
+
+
+    @Test
+    public void testConstructorCanAttackElementTypesEmpty() {
+        String name = "Svärd 12";
+        String desc = "Det tolfte svärdet som skapades";
+        int baseDamage = 10;
+        int range = 1;
+        ElementType[] canAttack = {};
+
+        assertThrows( IllegalArgumentException.class, () -> {
+            new Weapon(name, desc, baseDamage, range, canAttack);
+        } );
     }
 
 
