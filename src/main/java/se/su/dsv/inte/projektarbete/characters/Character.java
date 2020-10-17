d9559a3a48a735baa8043f49a6b98ac3ca4eb120
Michael Foussianis <mifo8834@git.dsv.su.se>
2020-10-17 15:42:24 +0200

    lade till klasser Character och Neutral

---
+++ src/main/java/se/su/dsv/inte/projektarbete/characters/Character.java
@@ -0,0 +1,47 @@
+package se.su.dsv.inte.projektarbete.characters;
+
+public abstract class Character {
+    private String name;
+    private int health;
+    private ThreatLevel threatLevel;
+    public enum ThreatLevel {
+        HOSTILE,
+        NEUTRAL,
+        FRIENDLY
+    }
+
+    public Character(String name, ThreatLevel threatLevel) {
+        health = 100;
+        this.name = name;
+        this.threatLevel = threatLevel;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public ThreatLevel getThreatLevel() {
+        return threatLevel;
+    }
+
+    public void setThreatLevel(ThreatLevel threatLevel) {
+        this.threatLevel = threatLevel;
+    }
+
+    public void Attack() {
+
+    }
+
+    private int CalculateDamage() {
+        int damage = 0;
+        return damage;
+    }
+
+    /*public Item loot() {
+        return Item;
+    }*/
+}
