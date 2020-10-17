3eb52cd4121d840aaddec2546c5ed38ea6515712
zachath <zachariasthorell@gmail.com>
2020-10-17 10:21:41 +0200

    Removed unused import

--- src/main/java/se/su/dsv/inte/projektarbete/map/Point.java
+++ src/main/java/se/su/dsv/inte/projektarbete/map/Point.java
@@ -1,7 +1,5 @@
 package se.su.dsv.inte.projektarbete.map;
 
-import java.util.logging.Logger;
-
 public class Point {
     private final TileType type;
     //TODO: private INSERT MONSTER / PLAYER TYPE HERE //Can only hold 1 at a time
