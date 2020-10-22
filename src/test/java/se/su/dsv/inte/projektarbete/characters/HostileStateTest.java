8f262afc1031a98ad959eda00d8b374e4cf9d92b
Michael Foussianis <mifo8834@git.dsv.su.se>
2020-10-22 19:26:52 +0200

    Refactored HostileStateTest to be more independent
    # Conflicts:
    #	src/test/java/se/su/dsv/inte/projektarbete/characters/CharacterTest.java

--- src/test/java/se/su/dsv/inte/projektarbete/characters/HostileStateTest.java
+++ src/test/java/se/su/dsv/inte/projektarbete/characters/HostileStateTest.java
@@ -1,5 +1,6 @@
 package se.su.dsv.inte.projektarbete.characters;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import se.su.dsv.inte.projektarbete.ElementType;
 import se.su.dsv.inte.projektarbete.map.Map;
@@ -11,38 +12,46 @@ import java.util.HashSet;
 import static org.junit.jupiter.api.Assertions.*;
 
 class HostileStateTest {
-    private static final CharacterStateController NEUTRAL_CONTROLLER = new CharacterStateController(StateType.NEUTRAL);
-    private static final State NEUTRAL_STATE = new NeutralState(NEUTRAL_CONTROLLER);
-    private static final State HOSTILE_STATE = new HostileState(NEUTRAL_CONTROLLER);
-    private static final State DEAD_STATE = new DeadState(NEUTRAL_CONTROLLER);
-    private static final State CHASING_STATE = new ChasingState(NEUTRAL_CONTROLLER);
-    private static final Map MAP = new Map(20, 20);
-    private static final Weapon WEAPON = new Weapon("sword", "super shiny", 10, 2, new HashSet<>(Arrays.asList(ElementType.LAND)));
-    private static final NonPlayerCharacter c1 = new NonPlayerCharacter("Bob", null, WEAPON, StateType.NEUTRAL);
-    private static final NonPlayerCharacter c2 = new NonPlayerCharacter("Bobby", null, WEAPON, StateType.NEUTRAL);
+    private CharacterStateController neutralController;
+    private Map map;
+    private Weapon weapon;
+    private NonPlayerCharacter c1;
+    private NonPlayerCharacter c2;
+
+    @BeforeEach
+    void testSetup() {
+        neutralController = new CharacterStateController(StateType.NEUTRAL);
+        map = new Map(30, 30);
+        weapon = new Weapon("sword", "super shiny", 10, 2, new HashSet<>(Arrays.asList(ElementType.LAND)));
+        c1 = new NonPlayerCharacter("Bob", null, weapon, StateType.NEUTRAL);
+        c2 = new NonPlayerCharacter("Bobby", null, weapon, StateType.NEUTRAL);
+    }
 
     @Test
     void attackWithinRange() {
-        MAP.placeCharacter(c1, 1, 1);
-        MAP.placeCharacter(c2, 2, 2);
+        State hostileState = new HostileState(neutralController);
+        map.placeCharacter(c1, 1, 1);
+        map.placeCharacter(c2, 2, 2);
         c1.getController().attack(c1, c2);
-        assertEquals(HOSTILE_STATE.toString(), c1.getController().getCurrentState().toString());
+        assertEquals(hostileState.toString(), c1.getController().getCurrentState().toString());
     }
 
     @Test
     void attackWithoutRangeTriggersChase() {
-        MAP.placeCharacter(c1, 1, 1);
-        MAP.placeCharacter(c2, 4, 4);
+        State chasingState = new ChasingState(neutralController);
+        map.placeCharacter(c1, 1, 1);
+        map.placeCharacter(c2, 4, 4);
         c1.getController().attack(c1, c2);
-        assertEquals(CHASING_STATE.toString(), c1.getController().getCurrentState().toString());
+        assertEquals(chasingState.toString(), c1.getController().getCurrentState().toString());
     }
 
     @Test
     void attackWithoutVisibilityTriggersNeutral() {
-        MAP.placeCharacter(c1, 1, 1);
-        MAP.placeCharacter(c2, 7, 7);
+        State neutralState = new NeutralState(neutralController);
+        map.placeCharacter(c1, 1, 1);
+        map.placeCharacter(c2, 7, 7);
         c1.getController().attack(c1, c2);
-        assertEquals(NEUTRAL_STATE.toString(), c1.getController().getCurrentState().toString());
+        assertEquals(neutralState.toString(), c1.getController().getCurrentState().toString());
     }
 
     @Test
